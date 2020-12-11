f4430ee10d44bedcb13914773d80fffcaf327b7c
Mario A. Crespo Reyes <maoacr@gmail.com>
2020-12-11 02:52:08 -0500

    Variables y constantes

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,15 @@
+//Variables y constantes
+
+const nombre = 'Mario';
+const apellido = 'Crespo';
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+  let valorDado = 6;
+  const nombre = 'Alejandro';
+  console.log(valorDado);
+  console.log(nombre);
+}
+console.log(valorDado);
