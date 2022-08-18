e024a8430b1e3d51168d1af002c72f2b02dc5876
Mario Alejandro Crespo Reyes <maoacr@gmail.com>
2022-08-18 18:39:25 -0500

    Comentando archivo de variables y funciones de JavaScript

--- 02-intro-javascript/src/bases/01-const-let.js
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -1,15 +1,21 @@
 //Variables y constantes
 
+//const como palabra reservada para valores que NO van a cambiar NUNCA durante la ejecucion de nuestro programa
 const nombre = 'Mario';
 const apellido = 'Crespo';
+
+//let como palabra reservada para valores que SI pueden cambiar durante la ejecucion de nuestro programa
 let valorDado = 5;
 valorDado = 4;
+
 console.log(nombre, apellido, valorDado);
 
 if (true) {
+  //Este tipo de valores son Block Scope (ES6)
   let valorDado = 6;
   const nombre = 'Alejandro';
   console.log(valorDado);
   console.log(nombre);
 }
+
 console.log(valorDado);
